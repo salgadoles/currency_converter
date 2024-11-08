dddecba3fef8d697d840e9e9f59cad961b8a423e
Samuel Lima do Amaral <140810343+salgadoles@users.noreply.github.com>
2024-11-07 21:59:05 -0300

    Creating first screen with customtkinter.

---
+++ steam.py
@@ -0,0 +1,18 @@
+# janela -> 500x500
+#titulo - conversor de moedas
+# campos de selecionar moedas de origem e destino (campos de listas)
+#botao de converter
+#lista de exibcao com os nomes das moedas
+
+import customtkinter
+
+#criar e configurar a janela
+janela = customtkinter.CTk()
+janela.geometry("500x500")
+
+#criar os botoes, textos e outros elementos
+
+# colocar todos os elementos na tela
+
+# rodar a janela
+janela.mainloop()
