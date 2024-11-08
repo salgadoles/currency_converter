f38238ad3ef04a364e6bb582c61d8f81169529a8
Samuel <salgabrowndazn@gmail.com>
2024-11-08 12:09:23 -0300

    Customizing Window

--- steam.py
+++ steam.py
@@ -7,11 +7,15 @@
 import customtkinter
 
 #criar e configurar a janela
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 janela = customtkinter.CTk()
 janela.geometry("500x500")
 
 #criar os botoes, textos e outros elementos
+titulo = customtkinter.CTkLabel(janela)
 
+textoMoedaOrigem = customtkinter.CTkLabel(janela)
 # colocar todos os elementos na tela
 
 # rodar a janela
